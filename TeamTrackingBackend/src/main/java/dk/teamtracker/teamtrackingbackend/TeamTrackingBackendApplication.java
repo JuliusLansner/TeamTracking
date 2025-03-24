cee0ebf79e3424f378f3947bf3ee1eb4dd7cbae2
Julius <axiiss@live.dk>
2025-03-24 13:32:32 +0100

    init

---
+++ TeamTrackingBackend/src/main/java/dk/teamtracker/teamtrackingbackend/TeamTrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package dk.teamtracker.teamtrackingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTrackingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTrackingBackendApplication.class, args);
+    }
+
+}
---
+++ TeamTrackingBackend/src/test/java/dk/teamtracker/teamtrackingbackend/TeamTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.teamtracker.teamtrackingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTrackingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
