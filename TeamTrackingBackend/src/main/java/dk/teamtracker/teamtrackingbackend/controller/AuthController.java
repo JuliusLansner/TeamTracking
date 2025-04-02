286e5f5d23e6840b13753739d74226c03e65433a
Julius <axiiss@live.dk>
2025-04-02 13:37:27 +0200

    Added more on AuthController.java

--- TeamTrackingBackend/src/main/java/dk/teamtracker/teamtrackingbackend/controller/AuthController.java
+++ TeamTrackingBackend/src/main/java/dk/teamtracker/teamtrackingbackend/controller/AuthController.java
@@ -1,17 +1,42 @@
 package dk.teamtracker.teamtrackingbackend.controller;
 
+import dk.teamtracker.teamtrackingbackend.dto.JwtAuthenticationResponse;
+import dk.teamtracker.teamtrackingbackend.dto.LoginRequest;
+import dk.teamtracker.teamtrackingbackend.util.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-
+import jakarta.validation.Valid;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
 
 
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.generateToken(authentication);
 
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
 
 }
---
+++ TeamTrackingBackend/src/main/java/dk/teamtracker/teamtrackingbackend/util/JwtTokenProvider.java
@@ -0,0 +1,33 @@
+package dk.teamtracker.teamtrackingbackend.util;
+
+import io.jsonwebtoken.Jwts;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+    @Value("${app.jwtExpirationInMs}")
+    private int jwtExpirationInMs;
+
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+}
